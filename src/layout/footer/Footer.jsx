cca9def227b8ceab9f46f193e6f2be63a4ac495e
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2023-06-30 00:42:28 +0600

    final commit

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,13 +1,15 @@
+import './footer.css';
+
+import { Col, Container, Row } from 'react-bootstrap';
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+
 import React from 'react';
 
 // import bootstrap components
-import { Col, Container, Row } from 'react-bootstrap';
 
 // import custom css
-import './footer.css';
 
 // icons import
-import {FaFacebookF, FaTwitter, FaLinkedinIn} from 'react-icons/fa';
 
 export default function Footer() {
   return (
@@ -16,27 +18,40 @@ export default function Footer() {
         <div className="footer-top">
           <Row>
             {/* logo col */}
-            <Col xs={12} md={7} lg={4} className='mb-4 mb-md-0'>
-              <p className='logo mb-3 mb-md-4'>
-                <a href="/f" className="text-uppercase font-integralcfBold font-700">nfters</a>
+            <Col xs={12} md={7} lg={4} className="mb-4 mb-md-0">
+              <p className="logo mb-3 mb-md-4">
+                <a
+                  href="/f"
+                  className="text-uppercase font-integralcfBold font-700"
+                >
+                  nfters
+                </a>
               </p>
               <p className="comp-details">
-                The world’s first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items.
+                The world’s first and largest digital marketplace for crypto
+                collectibles and non-fungible tokens (NFTs). Buy, sell, and
+                discover exclusive digital items.
               </p>
               <ul className="social-icons d-flex gap-2">
                 <li>
-                  <a href="#f" className='fb'><FaFacebookF /></a>
+                  <a href="#f" className="fb">
+                    <FaFacebookF />
+                  </a>
                 </li>
                 <li>
-                  <a href="#f" className='twtr'><FaTwitter /></a>
+                  <a href="#f" className="twtr">
+                    <FaTwitter />
+                  </a>
                 </li>
                 <li>
-                  <a href="#f" className='linkedin'><FaLinkedinIn /></a>
+                  <a href="#f" className="linkedin">
+                    <FaLinkedinIn />
+                  </a>
                 </li>
               </ul>
             </Col>
             {/* market place col */}
-            <Col xs={6} md={5} lg={2} className='mb-4 mb-md-0'>
+            <Col xs={6} md={5} lg={2} className="mb-4 mb-md-0">
               <h6 className="col-headings mb-3 mb-md-4">Market Place</h6>
               <ul className="link-items">
                 <li>
@@ -63,7 +78,7 @@ export default function Footer() {
               </ul>
             </Col>
             {/* my account col */}
-            <Col xs={6} md={5} lg={2} className='mb-4 mb-md-0'>
+            <Col xs={6} md={5} lg={2} className="mb-4 mb-md-0">
               <h6 className="col-headings mb-3 mb-md-4">My Account</h6>
               <ul className="link-items">
                 <li>
@@ -81,24 +96,34 @@ export default function Footer() {
               </ul>
             </Col>
             {/* subs. col */}
-            <Col xs={12} md={7} lg={4} className='mb-4 mb-md-0'>
+            <Col xs={12} md={7} lg={4} className="mb-4 mb-md-0">
               <h6 className="col-headings mb-3 mb-md-4">Stay in the loop</h6>
               <p className="subs-details mb-3 mb-md-4">
-                Join our mailing list to stay in the loop with our newest feature releases, NFT drops, and tips and tricks for navigating NFTs.
+                Join our mailing list to stay in the loop with our newest
+                feature releases, NFT drops, and tips and tricks for navigating
+                NFTs.
               </p>
               <form className="subscribe-form">
                 <div className="position-relative">
-                  <input type="email" className='form-control' placeholder='Enter your email address..' />
-                  <button className="btn btn-blue-lg subs-btn position-absolute">Subscibe Now</button>
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter your email address.."
+                  />
+                  <button className="btn btn-blue-lg subs-btn position-absolute">
+                    Subscibe Now
+                  </button>
                 </div>
               </form>
             </Col>
           </Row>
         </div>
         <div className="footer-bottom">
-          <p className="text-center font-averta-demo">Copyright &copy; 2022 Avi Yansah</p>
+          <p className="text-center font-averta-demo">
+            Copyright &copy; 2022 Avi Yansah
+          </p>
         </div>
       </Container>
     </footer>
-  )
+  );
 }
